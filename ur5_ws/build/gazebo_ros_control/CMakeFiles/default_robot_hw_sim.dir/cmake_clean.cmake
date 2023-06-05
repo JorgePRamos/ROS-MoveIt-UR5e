43aac16a14e7621fcc23e290d8f15a3b4b479fb2
Jorge Pérez Ramos <jorgeperezramos@usal.es>
2023-06-05 17:04:44 +0200

    Progress on gazebo controllers

---
+++ ur5_ws/build/gazebo_ros_control/CMakeFiles/default_robot_hw_sim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/devel/lib/libdefault_robot_hw_sim.pdb"
+  "/home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/devel/lib/libdefault_robot_hw_sim.so"
+  "CMakeFiles/default_robot_hw_sim.dir/src/default_robot_hw_sim.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/default_robot_hw_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ur5_ws/build/gazebo_ros_control/CMakeFiles/gazebo_ros_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/devel/lib/libgazebo_ros_control.pdb"
+  "/home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/devel/lib/libgazebo_ros_control.so"
+  "CMakeFiles/gazebo_ros_control.dir/src/gazebo_ros_control_plugin.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gazebo_ros_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
