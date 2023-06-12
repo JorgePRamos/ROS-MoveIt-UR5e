9c0a1d09e8e5692def32715ed336a826ee1c2a97
Jorge Pérez Ramos <jorgeperezramos@usal.es>
2023-06-12 15:43:24 +0200

    Finished empty framme

---
+++ ur5_ws/build/ur5e2f_hang/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/src/ur5e2f_hang
+# Build directory: /home/bicrobotics/ROS-MoveIt-UR5e/ur5_ws/build/ur5e2f_hang
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
